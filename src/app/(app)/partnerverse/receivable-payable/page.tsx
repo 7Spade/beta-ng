cc056ac9947e9bcbb24592be86812e4c4f6f2a4d
ac (𓂸) <ac7x7x@gmail.com>
2025-08-22 15:43:18 +0000

    1

--- src/app/(app)/partnerverse/receivable-payable/page.tsx
+++ src/app/(app)/partnerverse/receivable-payable/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { ReceivablePayableSystem } from '@/components/partnerverse/receivable-payable/receivable-payable-system'
+import { ReceivablePayableSystem } from '@/components/partnerverse/workflows/receivable-payable'
 
 export default function ReceivablePayablePage() {
   return <ReceivablePayableSystem />
